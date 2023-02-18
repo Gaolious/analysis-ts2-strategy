ef61744b9eb6c89f955e7b76e774ed39d825d592
aJava <gaolious@gmail.com>
2023-02-18 16:45:03 +0900

    contract

--- ts/app_root/strategies/managers.py
+++ ts/app_root/strategies/managers.py
@@ -353,7 +353,6 @@ def article_find_destination(version: RunVersion, article_id=None) -> Dict[int, 
     )
     location_id_set = set(PlayerQuest.objects.filter(
         version=version,
-        milestone=F('progress'),
     ).values_list('job_location__location_id', flat=True)) | {150, 151}
 
     if version.level_id < 25:
