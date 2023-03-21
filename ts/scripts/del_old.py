b50f619c3a65cf8e41626f45ffeb3ac168c8f1a2
aJava <gaolious@gmail.com>
2023-03-21 22:57:10 +0900

    add deleteing_old_data

---
+++ ts/scripts/del_old.py
@@ -0,0 +1,77 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
+from app_root.players.models import (
+    PlayerBuilding,
+    PlayerDestination,
+    PlayerFactory,
+    PlayerFactoryProductOrder,
+    PlayerJob,
+    PlayerContractList,
+    PlayerContract,
+    PlayerGift,
+    PlayerLeaderBoard,
+    PlayerLeaderBoardProgress,
+    PlayerTrain,
+    PlayerWarehouse,
+    PlayerWhistle,
+    PlayerWhistleItem,
+    PlayerAchievement,
+    PlayerDailyReward,
+    PlayerMap,
+    PlayerQuest,
+    PlayerVisitedRegion,
+    PlayerShipOffer,
+    PlayerCompetition,
+    PlayerUnlockedContent,
+    PlayerDailyOfferContainer,
+    PlayerDailyOffer,
+    PlayerDailyOfferItem,
+    PlayerCityLoopParcel,
+    PlayerCityLoopTask,
+)
+from app_root.servers.models import RunVersion
+
+
+def run(*args, **kwargs):
+    now = timezone.now() - timedelta(days=3)
+    version_id_list = list(
+        RunVersion.objects.filter(created__lte=now)
+        .order_by("id")
+        .values_list("id", flat=True)
+    )
+
+    MODEL_LIST = [
+        PlayerBuilding,
+        PlayerDestination,
+        PlayerFactory,
+        PlayerFactoryProductOrder,
+        PlayerJob,
+        PlayerContractList,
+        PlayerContract,
+        PlayerGift,
+        PlayerLeaderBoard,
+        PlayerLeaderBoardProgress,
+        PlayerTrain,
+        PlayerWarehouse,
+        PlayerWhistle,
+        PlayerWhistleItem,
+        PlayerAchievement,
+        PlayerDailyReward,
+        PlayerMap,
+        PlayerQuest,
+        PlayerVisitedRegion,
+        PlayerShipOffer,
+        PlayerCompetition,
+        PlayerUnlockedContent,
+        PlayerDailyOfferContainer,
+        PlayerDailyOffer,
+        PlayerDailyOfferItem,
+        PlayerCityLoopParcel,
+        PlayerCityLoopTask,
+    ]
+    for version_id in version_id_list:
+        for model in MODEL_LIST:
+            model.objects.filter(version_id=version_id).delete()
+        RunVersion.objects.filter(id=version_id).delete()
